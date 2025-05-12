10e7026c877b77f74dd7265cb450c693298d6052
Uippao <148978332+Uippao@users.noreply.github.com>
2025-05-12 19:08:57 +0300

    maybe now?

--- SpeedTests/KotlinNative/count.kt
+++ SpeedTests/KotlinNative/count.kt
@@ -1,11 +1,11 @@
-import kotlin.system.measureTimeMillis
+import kotlin.time.DurationUnit
 import kotlin.time.TimeSource
+import platform.posix.printf
 
 fun main() {
-    val start = TimeSource.Monotonic.markNow()  // Using TimeSource.Monotonic.markNow()
+    val mark = TimeSource.Monotonic.markNow()
     for (i in 1..10_000) println(i)
-    val elapsed = start.elapsedNow()  // Get the elapsed time
-
-    // Using String.format
-    println("Elapsed: %.9f seconds".format(elapsed.inWholeNanoseconds / 1e9))
+    val elapsed = mark.elapsedNow()
+    val seconds = elapsed.toDouble(DurationUnit.NANOSECONDS) / 1e9
+    printf("Elapsed: %.9f seconds\n", seconds)
 }
